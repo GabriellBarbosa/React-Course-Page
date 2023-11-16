f6b5f558b603b04a5a016e9751652bda37450c08
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-11-16 10:29:51 -0300

    agile software link

--- src/pages/Slide/CleanCode/CleanCode.tsx
+++ src/pages/Slide/CleanCode/CleanCode.tsx
@@ -11,7 +11,7 @@ function CleanCode() {
                 <div className={slideStyles.container}>
                     <span className={styles.logo}><img src={logo} alt="BookInVideo" /></span>
                     <h1 className={`${slideStyles.title} ${styles.introTitle}`}>Código Limpo</h1>
-                    <h2 className={slideStyles.subtitle}>Habilidades Práticas do Agile Software<span>.</span></h2>
+                    <h3 className={slideStyles.subtitle}>Habilidades Práticas do Agile Software<span>.</span></h3>
                 </div>
             </section>
 
@@ -21,19 +21,26 @@ function CleanCode() {
                     <div className={styles.manifestoImg}>
                         <img src={agileManifesto} alt="Manifesto para Desenvolvimento Ágil de Software" />
                     </div>
-                    <a className={styles.manifestoLink} href="https://agilemanifesto.org/iso/ptbr/manifesto.html" target='_blank'>https://agilemanifesto.org/iso/ptbr/manifesto.html</a>
+                    <a 
+                        className={`${slideStyles.externalLink} ${styles.externalLink}`} 
+                        href="https://agilemanifesto.org/iso/ptbr/manifesto.html" 
+                        target='_blank'
+                    >
+                        https://agilemanifesto.org/iso/ptbr/manifesto.html
+                    </a>
                 </div>
             </section>
 
             <section className={slideStyles.wrapper}>
                 <div className={slideStyles.container}>
-                    <h1 className={slideStyles.regular_title}>Agile Software</h1>
-                    <h2 className={slideStyles.subtitle}>Práticas Técnicas<span>.</span></h2>
-                    <ul className={slideStyles.list}>
+                    <h2 className={slideStyles.regular_title}>Agile Software</h2>
+                    <h3 className={slideStyles.subtitle}>Práticas Técnicas<span>.</span></h3>
+                    <ul className={`${slideStyles.list} ${styles.list}`}>
                         <li>Desenvolvimento Orientado a Testes.</li>
                         <li>Refatoração.</li>
                         <li>Design Simples.</li>
                     </ul>
+                    <p className={`${slideStyles.infoSource} ${styles.infoSource}`}>Desenvolvimento Ágil Limpo: De Volta às Origens.</p>
                 </div>
             </section>
         </div>
