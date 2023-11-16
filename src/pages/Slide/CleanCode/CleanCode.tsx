42430fcce864bde9093d10e0176928ee4ed627a0
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-11-16 10:11:18 -0300

    style list

--- src/pages/Slide/CleanCode/CleanCode.tsx
+++ src/pages/Slide/CleanCode/CleanCode.tsx
@@ -11,7 +11,7 @@ function CleanCode() {
                 <div className={slideStyles.container}>
                     <span className={styles.logo}><img src={logo} alt="BookInVideo" /></span>
                     <h1 className={`${slideStyles.title} ${styles.introTitle}`}>Código Limpo</h1>
-                    <p className={slideStyles.subtitle}>Habilidades Práticas do Agile Software<span>.</span></p>
+                    <h2 className={slideStyles.subtitle}>Habilidades Práticas do Agile Software<span>.</span></h2>
                 </div>
             </section>
 
@@ -22,6 +22,18 @@ function CleanCode() {
                         <img src={agileManifesto} alt="Manifesto para Desenvolvimento Ágil de Software" />
                     </div>
                     <a className={styles.manifestoLink} href="https://agilemanifesto.org/iso/ptbr/manifesto.html" target='_blank'>https://agilemanifesto.org/iso/ptbr/manifesto.html</a>
+                </div>
+            </section>
+
+            <section className={slideStyles.wrapper}>
+                <div className={slideStyles.container}>
+                    <h1 className={slideStyles.regular_title}>Agile Software</h1>
+                    <h2 className={slideStyles.subtitle}>Práticas Técnicas<span>.</span></h2>
+                    <ul className={slideStyles.list}>
+                        <li>Desenvolvimento Orientado a Testes.</li>
+                        <li>Refatoração.</li>
+                        <li>Design Simples.</li>
+                    </ul>
                 </div>
             </section>
         </div>
