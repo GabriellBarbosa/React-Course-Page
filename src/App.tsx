bc56a4b997a3c8ccfe5725c4375daa36f5b44905
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-11-30 15:24:22 -0300

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Slide from './pages/Slide/Slide';
 import './App.css'
 
 function App() {
-  return (
-    <Routes>
-      <Route path='slide/*' element={<Slide />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path='slide/*' element={<Slide />} />
+        </Routes>
+    )
 }
 
-export default App
+export default App;
