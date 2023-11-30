659d77305750361257a67484bcc8cbd65467282c
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-11-30 14:48:30 -0300

    improve routes organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import Slide from './pages/Slide/Slide';
-import CleanCode from './pages/Slide/CleanCode/CleanCode';
 import './App.css'
 
 function App() {
   return (
     <Routes>
-      <Route path='slide' element={<Slide />} >
-        <Route path="clean-code" element={<CleanCode />} />
-      </Route>
+      <Route path='slide/*' element={<Slide />} />
     </Routes>
   )
 }
--- src/pages/Slide/Slide.tsx
+++ src/pages/Slide/Slide.tsx
@@ -1,9 +1,12 @@
-import { Outlet } from "react-router-dom";
-
+import { Routes, Route, Outlet } from 'react-router-dom'
+import CleanCode from './CleanCode/CleanCode';
 
 function Slide() {
     return (
         <>
+            <Routes>
+                <Route path="clean-code" element={<CleanCode />} />
+            </Routes>
             <Outlet />
         </>
     );
