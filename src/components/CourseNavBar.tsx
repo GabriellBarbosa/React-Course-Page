fed21f4f97e5cb2a442ad75e0700f5c00dc34780
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-11-30 21:43:49 -0300

    course sidenav layout

--- src/components/CourseNavBar.tsx
+++ src/components/CourseNavBar.tsx
@@ -5,11 +5,11 @@ function CourseNavBar() {
         <div className={styles.wrapper}>
             <div className={styles.header}>
                 <h1 className={styles.title}>Código Limpo</h1>
-                <span aria-label="mostrar ou esconder o menu"></span>
+                <span className={styles.toggle_navbar_btn} aria-label="mostrar ou esconder o menu"></span>
             </div>
             <div className={styles.charpter}>
                 <h2 className={styles.title}>01 Introdução</h2>
-                <ul>
+                <ul className={styles.video_list}>
                     <li className={styles.video}>
                         <div className={styles.video_name_wrapper}>
                             <span className={styles.video_number}>0102</span>
@@ -17,7 +17,17 @@ function CourseNavBar() {
                         </div>
                         <div className={styles.video_duration_wrapper}>
                             <p className={styles.video_duration}>19:55</p>
-                            <span aria-label='vídeo já assistido'></span>
+                            <span className={styles.video_watched_feedback} aria-label='vídeo já assistido'></span>
+                        </div>
+                    </li>
+                    <li className={styles.video}>
+                        <div className={styles.video_name_wrapper}>
+                            <span className={styles.video_number}>0102</span>
+                            <p className={styles.video_name}>O que é código limpo</p>
+                        </div>
+                        <div className={styles.video_duration_wrapper}>
+                            <p className={styles.video_duration}>19:55</p>
+                            <span className={styles.video_watched_feedback} aria-label='vídeo já assistido'></span>
                         </div>
                     </li>
                 </ul>
