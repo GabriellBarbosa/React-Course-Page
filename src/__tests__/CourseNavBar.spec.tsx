eebbbb2ad376bb8b8d293499e46c29eeb46ca06a
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-12-02 14:51:10 -0300

    deactive navbar on background click

--- src/__tests__/CourseNavBar.spec.tsx
+++ src/__tests__/CourseNavBar.spec.tsx
@@ -11,7 +11,7 @@ describe('CourseNavBar', () => {
         });
     });
 
-    it('active navbar', () => {
+    it('active navbar on button click', () => {
         const navbar = screen.getByTestId('navbar');
         const navbarButton = screen.getByTestId('toggleActive');
 
@@ -22,7 +22,7 @@ describe('CourseNavBar', () => {
         expect([...navbar.classList]).toContain(styles.active);
     });
 
-    it('deactive navbar', () => {
+    it('deactive navbar on button click', () => {
         const navbar = screen.getByTestId('navbar');
         const navbarButton = screen.getByTestId('toggleActive');
 
@@ -32,6 +32,18 @@ describe('CourseNavBar', () => {
 
         fireEvent.click(navbarButton);
 
+        expect([...navbar.classList]).not.toContain(styles.active);
+    });
+
+    it('deactive navbar on background click', () => {
+        const navbar = screen.getByTestId('navbar');
+        const navbarButton = screen.getByTestId('toggleActive');
+        const background = screen.getByTestId('background');
+
+        fireEvent.click(navbarButton);
+        expect([...navbar.classList]).toContain(styles.active);
+
+        fireEvent.click(background);
         expect([...navbar.classList]).not.toContain(styles.active);
     });
 })
--- src/components/CourseNavBar.tsx
+++ src/components/CourseNavBar.tsx
@@ -3,51 +3,62 @@ import React from 'react';
 
 function CourseNavBar() {
     const [navbarActive, setNavbarActive] = React.useState(false);
-
+    
     function toggleNavbarActive() {
         setNavbarActive(!navbarActive);
     }
 
+    function deactiveNavbar() {
+        setNavbarActive(false);
+    }
+
     return (
-        <div 
-            className={`${styles.navbar} ${navbarActive ? styles.active : ''}`} 
-            data-testid="navbar"
-        >
-            <div className={styles.header}>
-                <h1 className={styles.title}>Código Limpo</h1>
-                <span 
-                    className={styles.toggle_navbar_btn} 
-                    aria-label="mostrar ou esconder o menu" 
-                    data-testid="toggleActive"
-                    onClick={toggleNavbarActive}
-                ></span>
+        <>
+            <div 
+                data-testid="background" 
+                className={`${styles.background} ${navbarActive ? styles.active : ''}`}
+                onClick={deactiveNavbar} 
+            ></div>
+            <div 
+                className={`${styles.navbar} ${navbarActive ? styles.active : ''}`} 
+                data-testid="navbar"
+            >
+                <div className={styles.header}>
+                    <h1 className={styles.title}>Código Limpo</h1>
+                    <span 
+                        className={styles.toggle_navbar_btn} 
+                        aria-label="mostrar ou esconder o menu" 
+                        data-testid="toggleActive"
+                        onClick={toggleNavbarActive}
+                    ></span>
+                </div>
+                <div className={styles.charpter}>
+                    <h2 className={styles.title}>01 Introdução</h2>
+                    <ul className={styles.video_list}>
+                        <li className={styles.video}>
+                            <div className={styles.video_name_wrapper}>
+                                <span className={styles.video_number}>0102</span>
+                                <p className={styles.video_name}>O que é código limpo</p>
+                            </div>
+                            <div className={styles.video_duration_wrapper}>
+                                <p className={styles.video_duration}>19:55</p>
+                                <span className={styles.video_watched_feedback} aria-label='vídeo já assistido'></span>
+                            </div>
+                        </li>
+                        <li className={styles.video}>
+                            <div className={styles.video_name_wrapper}>
+                                <span className={styles.video_number}>0102</span>
+                                <p className={styles.video_name}>O que é código limpo</p>
+                            </div>
+                            <div className={styles.video_duration_wrapper}>
+                                <p className={styles.video_duration}>19:55</p>
+                                <span className={styles.video_watched_feedback} aria-label='vídeo já assistido'></span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className={styles.charpter}>
-                <h2 className={styles.title}>01 Introdução</h2>
-                <ul className={styles.video_list}>
-                    <li className={styles.video}>
-                        <div className={styles.video_name_wrapper}>
-                            <span className={styles.video_number}>0102</span>
-                            <p className={styles.video_name}>O que é código limpo</p>
-                        </div>
-                        <div className={styles.video_duration_wrapper}>
-                            <p className={styles.video_duration}>19:55</p>
-                            <span className={styles.video_watched_feedback} aria-label='vídeo já assistido'></span>
-                        </div>
-                    </li>
-                    <li className={styles.video}>
-                        <div className={styles.video_name_wrapper}>
-                            <span className={styles.video_number}>0102</span>
-                            <p className={styles.video_name}>O que é código limpo</p>
-                        </div>
-                        <div className={styles.video_duration_wrapper}>
-                            <p className={styles.video_duration}>19:55</p>
-                            <span className={styles.video_watched_feedback} aria-label='vídeo já assistido'></span>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        </>
     );
 }
 
