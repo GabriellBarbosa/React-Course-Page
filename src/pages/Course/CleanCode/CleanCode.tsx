90c4a2f252669fc0b1ebc98694fd2308a017e580
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-12-02 13:16:24 -0300

    add video player

--- src/pages/Course/CleanCode/CleanCode.tsx
+++ src/pages/Course/CleanCode/CleanCode.tsx
@@ -15,6 +15,14 @@ function CleanCode() {
                     <a href="/slide/clean-code" className={styles.slide_link}>Slide</a>
                 </div>
 
+                <div className={styles.video_player}>
+                    <iframe 
+                        src="https://player.vimeo.com/video/890014305?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479" 
+                        allow="autoplay; fullscreen; picture-in-picture" 
+                        className={styles.iframe} title="Sequência 01_1"></iframe>
+                </div>
+                <script src="https://player.vimeo.com/api/player.js"></script>
+
                 <div className={styles.change_video_button_wrapper}>
                     <button className={styles.prev}>Anterior</button>
                     <button className={styles.next}>Próximo</button>
