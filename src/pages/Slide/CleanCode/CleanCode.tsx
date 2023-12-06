2599bb7adf75ff4dc15125e5644cbc57e879909a
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-12-06 11:27:30 -0300

    organize slide routes

--- src/pages/Slide/CleanCode/CleanCode.tsx
+++ src/pages/Slide/CleanCode/CleanCode.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route, Outlet } from 'react-router-dom'
+import _0102 from '../CleanCode/0102';
 
 function CleanCode() {
     return (
         <>
             <Routes>
-                <Route path="0102-codigo-limpo" element={<CleanCode />} />
+                <Route path="0102-codigo-limpo" element={<_0102 />} />
             </Routes>
             <Outlet />
         </>
--- src/pages/Slide/Slide.tsx
+++ src/pages/Slide/Slide.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route, Outlet } from 'react-router-dom'
-import CleanCode from './CleanCode/0102';
+import CleanCode from './CleanCode/CleanCode';
 
 function Slide() {
     return (
         <>
             <Routes>
-                <Route path="codigo-limpo" element={<CleanCode />} />
+                <Route path="codigo-limpo/*" element={<CleanCode />} />
             </Routes>
             <Outlet />
         </>
