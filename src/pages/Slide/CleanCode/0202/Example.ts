1ebdc53a4afcf175ac2f4c9034790f891cf3ebd4
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-12-08 11:30:14 -0300

    add context slide

--- src/pages/Slide/CleanCode/0202/Example.ts
+++ src/pages/Slide/CleanCode/0202/Example.ts
@@ -1,13 +1,68 @@
 const example1 = 
 `clients.map(i => {
-    if (i.name.toUpperCase().indexOf(name.toUpperCase()) > -1)...`;
+    if (i.name.toUpperCase().indexOf(name.toUpperCase()) > -1) {
+
+        let sum = 0;
+        i.enterprises.map((j) => {
+            sum = sum + parseInt(j.realties);
+        });
+
+        array.push({
+            _id: i._id,
+            name: i.name,
+            total_enterprises: \`\${i.enterprises.length}\`,
+            total_realties: \`\${sum}\`
+        });
+    }
+})`;
 
 const example1Resolved = 
 `clients.map(client => {
-    if (client.name.toUpperCase().indexOf(name.toUpperCase()) > -1)...`;
+    if (client.name.toUpperCase().indexOf(name.toUpperCase()) > -1) {
+
+        let sum = 0;
+        client.enterprises.map((j) => {
+            sum = sum + parseInt(j.realties);
+        });
+
+        array.push({
+            _id: client._id,
+            name: client.name,
+            total_enterprises: \`\${client.enterprises.length}\`,
+            total_realties: \`\${sum}\`
+        });
+    }
+})`;
+
+const example2 = 
+`client.enterprises.map((j) => {
+    sum = sum + parseInt(j.realties);
+})`;
+
+const example2Resolved = 
+`client.enterprises.map((enterprise) => {
+    sum = sum + parseInt(enterprise.realties);
+})`
+
+
+const example3 = 
+`let sum = 0;
+client.enterprises.map((enterprise) => {
+  sum = sum + parseInt(enterprise.realties);
+})`;
+
+const example3Resolved = 
+`let sumOfRealties = 0;
+client.enterprises.map((enterprise) => {
+  sumOfRealties = sumOfRealties + parseInt(enterprise.realties);
+})`
 
 
 export {
     example1,
     example1Resolved,
+    example2,
+    example2Resolved,
+    example3,
+    example3Resolved,
 }
--- src/pages/Slide/CleanCode/0202/index.tsx
+++ src/pages/Slide/CleanCode/0202/index.tsx
@@ -7,7 +7,11 @@ import useScrollbar from '../../../../hooks/useScrollbar';
 import CodeEditor from '../../../../components/CodeEditor';
 import { 
     example1, 
-    example1Resolved 
+    example1Resolved, 
+    example2,
+    example2Resolved,
+    example3,
+    example3Resolved
 } from './Example';
 
 import { default as logo } from '../../../../assets/BookInVideo.svg';
@@ -54,6 +58,52 @@ function _0202() {
                                 Então mudamos para “<span className={styles.code_hightlight}>client</span>”.
                             </p>
                             <CodeEditor code={example1Resolved} />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className={slideStyles.slide} data-testid="slide" ref={addSlide}>
+                <div className={slideStyles.container}>
+                    <h2 className={styles.title}>Distinções Significativas</h2>
+                    <p className={styles.text}>
+                        Variáveis de loop i, j. Ter essas variáveis em um escopo grande faz 
+                        com que seja cansativo ter que lembrar o que essas variáveis são.
+                    </p>
+                    <div className={styles.code_examples_wrapper}>
+                        <div className={styles.code_example}>
+                            <p className={styles.code_description}>
+                                E também “<span className={styles.code_hightlight}>j</span>” não fazia mais sentido para nós.
+                            </p>
+                            <CodeEditor code={example2} />
+                        </div>
+                        <div className={styles.code_example}>
+                            <p className={styles.code_description}>
+                                Então mudamos para “<span className={styles.code_hightlight}>enterprise</span>”.
+                            </p>
+                            <CodeEditor code={example2Resolved} />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className={slideStyles.slide} data-testid="slide" ref={addSlide}>
+                <div className={slideStyles.container}>
+                    <h2 className={styles.title}>Adicione Contexto</h2>
+                    <p className={styles.text}>
+                        Adicione contexto em nomes genéricos. Quando tivermos os contextos bem definidos 
+                        podemos agrupar os elementos e remover contextos desnecessários.
+                    </p>
+                    <div className={styles.code_examples_wrapper}>
+                        <div className={styles.code_example}>
+                            <p className={styles.code_description}>
+                                A variável “<span className={styles.code_hightlight}>sum</span>” não informava o contexto.
+                            </p>
+                            <CodeEditor code={example3} />
+                        </div>
+                        <div className={styles.code_example}>
+                            <p className={styles.code_description}>
+                                Então mudamos para “<span className={styles.code_hightlight}>sumOfRealties</span>”.
+                            </p>
+                            <CodeEditor code={example3Resolved} />
                         </div>
                     </div>
                 </div>
