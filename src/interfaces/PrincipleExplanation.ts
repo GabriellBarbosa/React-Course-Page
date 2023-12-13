570d937c4c550f45da4f8d0a28de098ec478a142
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-12-13 15:22:06 -0300

    rename interface

---
+++ src/interfaces/PrincipleExplanation.ts
@@ -0,0 +1,9 @@
+import CodeExample from "./CodeExample";
+
+interface PrincipleExplanation {
+    title: string;
+    description: string;
+    code: CodeExample
+}
+
+export default PrincipleExplanation;
