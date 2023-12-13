d76b57898b33e2b5ab5c9085222b33f93e8080db
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-12-13 15:13:55 -0300

    remove repeated css

--- src/components/CodeExplanationComponent.tsx
+++ src/components/CodeExplanationComponent.tsx
@@ -1,14 +1,14 @@
-import styles from './CodeExplanationComponent.module.css';
+import slideStyles from '../assets/css/Slide.module.css'
 import CodeEditor from './CodeEditor';
 import CodeExplanation from '../interfaces/CodeExplanation';
 
 function CodeExplanationComponent(props: CodeExplanation) {
     return (
         <>
-            <h2 className={styles.title}>{props.title}</h2>
-            <p className={styles.text}>{props.description}</p>
-            <div className={styles.code_example}>
-                <p className={styles.code_description}>{props.code.description}</p>
+            <h2 className={slideStyles.principleTitle}>{props.title}</h2>
+            <p className={slideStyles.principleDescription}>{props.description}</p>
+            <div className={slideStyles.principleCodeExamples}>
+                <p className={slideStyles.principleCodeDescription}>{props.code.description}</p>
                 <CodeEditor code={props.code.value} />
             </div>
         </>
--- src/components/RefactoringExplanationComponent.tsx
+++ src/components/RefactoringExplanationComponent.tsx
@@ -1,19 +1,20 @@
 import styles from './RefactoringExplanationComponent.module.css';
+import slideStyles from '../assets/css/Slide.module.css';
 import CodeEditor from './CodeEditor';
 import RefactoringExplanation from '../interfaces/RefactoringExplanation';
 
 function RefactoringExplanationComponent(props: RefactoringExplanation) {
     return (
         <div>
-            <h2 className={styles.title}>{props.title}</h2>
-            <p className={styles.text}>{props.description} </p>
-            <div className={styles.code_examples_wrapper}>
+            <h2 className={slideStyles.principleTitle}>{props.title}</h2>
+            <p className={slideStyles.principleDescription}>{props.description} </p>
+            <div className={slideStyles.principleCodeExamples}>
                 <div className={styles.code_example}>
-                    <p className={styles.code_description}>{props.codeBefore.description}</p>
+                    <p className={slideStyles.principleCodeDescription}>{props.codeBefore.description}</p>
                     <CodeEditor code={props.codeBefore.value} />
                 </div>
                 <div className={styles.code_example}>
-                    <p className={styles.code_description}>{props.codeAfter.description}</p>
+                    <p className={slideStyles.principleCodeDescription}>{props.codeAfter.description}</p>
                     <CodeEditor code={props.codeAfter.value} />
                 </div>
             </div>
