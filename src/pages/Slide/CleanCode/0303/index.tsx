9f1a19f18d26234c3108e28e3deb01b47b93e351
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2023-12-23 10:44:47 -0300

    add description to codes

--- src/pages/Slide/CleanCode/0303/index.tsx
+++ src/pages/Slide/CleanCode/0303/index.tsx
@@ -34,7 +34,7 @@ function _0301() {
             title: 'Nenhum Parâmetro',
             description: 'O melhor número de parâmetros é zero, assim a chamada da função fica muito simples.',
             code: {
-                description: <></>,
+                description: <>Exemplo de função sem parâmetros:</>,
                 value: example2
             },
         },
@@ -42,7 +42,7 @@ function _0301() {
             title: 'Um Parâmetro (Mônade)',
             description: 'Depois de nenhum parâmetro, a melhor opção é um parâmetro. Ainda com um parâmetro, a função consegue manter a simplicidade.',
             code: {
-                description: <></>,
+                description: <>Exemplo de função com um parâmetro:</>,
                 value: example3
             },
         },
@@ -50,7 +50,7 @@ function _0301() {
             title: 'Dois Parâmetros (Díade)',
             description: 'Há vários casos em que precisamos ter dois parâmetros, porém é importante que os dois parâmetros sejam parte do mesmo conceito.',
             code: {
-                description: <></>,
+                description: <>Exemplo de função com dois parâmetros:</>,
                 value: example4,
             },
         },
@@ -58,15 +58,15 @@ function _0301() {
             title: 'Três Parâmetros (Tríade)',
             description: 'Testar todas as possibilidades de uma função com três parâmetros é difícil. Avalie bem se você pode reduzir a quantidade de parâmetros nesses casos.',
             code: {
-                description: <></>,
+                description: <>Exemplo de função com três parâmetros:</>,
                 value: example5
             },
         },
         {
-            title: 'Mais de três parâmetros',
+            title: 'Mais de Três Parâmetros',
             description: 'Se você tem uma função com mais de três parâmetros, então vale a pena você conhecer uma refatoração chamada "introduzir objetos de parâmetro".',
             code: {
-                description: <></>,
+                description: <>Exemplo de função com mais de três parâmetros:</>,
                 value: example6
             },
         },
@@ -74,15 +74,15 @@ function _0301() {
             title: 'Lista Como Parâmetro',
             description: 'Uma lista como parâmetro pode ser considerada um parâmetro apenas, desde que os elementos da lista sejam tratados da mesma forma.',
             code: {
-                description: <></>,
+                description: <>O método "<span className={slideStyles.hightlight}>log</span>" do objeto "<span className={slideStyles.hightlight}>console</span>" recebe uma lista como paramêtro:</>,
                 value: example7
             },
         },
         {
             title: 'Parâmetros de Saída',
-            description: 'Parâmetros de saída é algo que devemos evitar, uma função que recebe um parâmetro, o altera e não retorna nada faz com que tenhamos de ler o código no mínimo duas vezes para entender o que está acontecendo.',
+            description: 'Uma função que recebe um parâmetro, o altera e não retorna nada, faz com que tenhamos de ler o código no mínimo duas vezes para entender o que está acontecendo.',
             code: {
-                description: <></>,
+                description: <>Exemplo de função que utiliza um parâmetro de saída:</>,
                 value: example9
             },
         },
@@ -90,7 +90,7 @@ function _0301() {
             title: 'Parâmetros de Entrada',
             description: 'Por padrão, esperamos que um objeto seja de entrada. Recebemos um parâmetro, fazemos algo e retornamos algum valor.',
             code: {
-                description: <></>,
+                description: <>Exemplo de função que utiliza um parâmetro de entrada:</>,
                 value: example8
             },
         },
@@ -101,11 +101,11 @@ function _0301() {
             title: 'Introduzir Objeto de Parâmetros',
             description: 'Elementos que possuem certa relação aparecem juntos em diversas partes do código. Agrupá-los em uma estrutura de dados é uma boa opção para manter esses elementos juntos e oferecê-los um contexto melhor.',
             codeBefore: {
-                description: <></>,
+                description: <>Exemplo de função com muitos parâmetros:</>,
                 value: example6
             },
             codeAfter: {
-                description: <></>,
+                description: <>Lista de parâmetros substituída por objeto:</>,
                 value: example6Resolved
             },
         },
@@ -113,11 +113,11 @@ function _0301() {
             title: 'Parâmetros Lógicos',
             description: 'Um parâmetro lógico, em muitos casos, é um bom sinal de que sua função está fazendo mais de uma coisa.',
             codeBefore: {
-                description: <></>,
+                description: <>Exemplo de função que utiliza um parâmetro booleano:</>,
                 value: example1
             },
             codeAfter: {
-                description: <></>,
+                description: <>A função foi quebrada em duas:</>,
                 value: example1Resolved
             },
         },
