cc04b472af3bb131bfc88e26fddc941fe3abc8aa
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2024-01-03 13:09:24 -0300

    0306 slide ok

---
+++ src/pages/Slide/CleanCode/0306/Example.ts
@@ -0,0 +1,46 @@
+const example1 = 
+`function calcularValorLiquido(valor, tipoPagamento) {
+    const taxa = calcularTaxa(valor, tipoPagamento);
+    return valor - taxa;
+}
+
+function calcularTaxa(valor, tipoPagamento) {
+    switch (tipoPagamento) {
+        case 'CRÉDITO': {
+            const taxaPorcentagem = 3.49;
+            const taxaEmReais = (valor * taxaPorcentagem) / 100;
+            return taxaEmReais;
+        } case 'BOLETO': {
+            const taxaFixaEmReais = 1.40;
+            const taxaIntermediacaoPorcentagem = 4.99;
+            const taxaIntermediacaoEmReais = (valor * taxaIntermediacaoPorcentagem) / 100;
+            const taxaEmReais = taxaIntermediacaoEmReais + taxaFixaEmReais;
+            return taxaEmReais;
+        }  case 'PIX':
+            return 0;
+        default:
+            throw new Error('meio de pagamento inválido');
+    }
+}`;
+
+const example2 =
+`function calcularValorLiquido(valor, meioDePagamento) {
+    const pagamento = criarMeioDePagemento(meioDePagamento);
+    const taxa = pagamento.calcularTaxa(valor);
+    return valor - taxa;
+}
+
+function criarMeioDePagemento(meioDePagamento) {
+    switch (meioDePagamento) {
+        case 'CRÉDITO':
+            return new Credito();
+        case 'BOLETO':
+            return new Boleto();
+        case 'PIX':
+            return new Pix();
+        default:
+            throw new Error('meio de pagamento inválido');
+    }
+}`;
+
+export { example1, example2 };
--- src/pages/Slide/CleanCode/0306/index.tsx
+++ src/pages/Slide/CleanCode/0306/index.tsx
@@ -1,7 +1,13 @@
 import slideStyles from '../../../../assets/css/Slide.module.css';
 import useSlide from '../../../../hooks/useSlide';
 import useScrollbar from '../../../../hooks/useScrollbar';
+import PrincipleExplanationComponent from '../../../../components/PrincipleExplanationComponent';
+import PrincipleExplanation from '../../../../interfaces/PrincipleExplanation';
 import React from 'react';
+import {
+    example1,
+    example2
+} from './Example';
 
 import { default as logo } from '../../../../assets/BookInVideo.svg';
 
@@ -13,6 +19,27 @@ function _0306() {
         hideScrollbar();
         return () => showScrollbar();
     }, []);
+
+    const principles: PrincipleExplanation[] = [
+        {
+            title: 'O que significa?',
+            description: 'Entidades de software devem ser abertas para extensão, mas fechadas para modificação.',
+            code: {
+                description: <>Exemplo de código que viola essa regra:</>,
+                value: example1,
+                showCopyButton: true
+            },
+        },
+        {
+            title: 'Substituir Condicional por Polimorfismo',
+            description: 'Criamos uma classe para cada meio de pagamento. Agora, o método de calcular a taxa será oferecido por sua respectiva classe.',
+            code: {
+                description: <>Código refatorado:</>,
+                value: example2,
+                showCopyButton: true
+            },
+        }
+    ];
 
     return (
         <div 
@@ -27,6 +54,29 @@ function _0306() {
                     <h3 className={slideStyles.subtitle}>0306 Capítulo: Funções<span>.</span></h3>
                 </div>
             </section>
+
+            {principles.map((principle) => {
+                return (
+                    <section 
+                        className={slideStyles.slide} 
+                        data-testid="slide" 
+                        ref={addSlide}
+                        key={principle.title}
+                    >
+                        <div className={slideStyles.container}>
+                            <PrincipleExplanationComponent
+                                title={principle.title}
+                                description={principle.description}
+                                code={{
+                                    description: principle.code.description,
+                                    value: principle.code.value,
+                                    showCopyButton: principle.code.showCopyButton
+                                }}
+                            />
+                        </div>
+                    </section>
+                )
+            })}
         </div>
     );
 }
