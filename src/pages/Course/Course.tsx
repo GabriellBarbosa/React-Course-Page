426d4403c41cfb700e1424e30fd62a97e4de142a
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2024-03-19 21:32:37 -0300

    using env to store api uri

--- src/pages/Course/Course.tsx
+++ src/pages/Course/Course.tsx
@@ -3,13 +3,19 @@ import Navbar from './components/Navbar';
 import Header from '../../components/Header';
 import Loading from './components/Loading';
 import { Course } from '../../interfaces/Course';
+import { useLocation } from "react-router-dom";
 
 function CourseComponent() {
     const [course, setCourse] = React.useState<Course | null>(null);
     const [courseNotFound, setCourseNotFound] = React.useState<boolean>(false);
+    const location = useLocation();
 
     React.useEffect(() => {
-        fetch('http://bookinvideo.local/wp-json/api/curso/codigo-limpo')
+      console.log('Location changed');
+    }, [location]);
+  
+    React.useEffect(() => {
+        fetch(import.meta.env.VITE_API_ENDPOINT + '/api/curso/codigo-limpo')
             .then(response => response.json())
             .then(json => setCourse(json))
             .catch(() => setCourseNotFound(true))
