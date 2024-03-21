9e1b90de796755c60e5629b39e1cf04e4960cb6a
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2024-03-20 22:42:57 -0300

    lesson page in progress

--- src/pages/Course/Course.tsx
+++ src/pages/Course/Course.tsx
@@ -1,23 +1,34 @@
+import styles from './Course.module.css';
 import React from "react";
 import Navbar from './components/Navbar';
 import Header from '../../components/Header';
 import Loading from './components/Loading';
 import { Course } from '../../interfaces/Course';
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+
+interface Lesson {
+    name: string;
+    sequence: string;
+    video_src: string;
+}
 
 function CourseComponent() {
     const [course, setCourse] = React.useState<Course | null>(null);
+    const [lesson, setLesson] = React.useState<Lesson | null>(null);
+
     const [courseNotFound, setCourseNotFound] = React.useState<boolean>(false);
+
+    const urlParams = useParams();
     const location = useLocation();
 
     React.useEffect(() => {
-        fetch(import.meta.env.VITE_API_ENDPOINT + '/curso/codigo-limpo/0101-configuracao')
+        fetch(import.meta.env.VITE_API_ENDPOINT + `/curso/${urlParams.course}/${urlParams.lesson}`)
             .then(response => response.json())
-            .then(json => console.log(json))
+            .then(json => setLesson(json))
     }, [location]);
   
     React.useEffect(() => {
-        fetch(import.meta.env.VITE_API_ENDPOINT + '/curso/codigo-limpo')
+        fetch(import.meta.env.VITE_API_ENDPOINT + `/curso/${urlParams.course}`)
             .then(response => response.json())
             .then(json => setCourse(json))
             .catch(() => setCourseNotFound(true))
@@ -32,6 +43,30 @@ function CourseComponent() {
                 ) : (
                     <>{ course ? <Navbar course={course} />  : <Loading /> }</>
                 )}
+            </div>
+            <div className={styles.container}>
+                <div className={styles.wrapper_title_slide}>
+                    <h2 className={styles.title}>{`${lesson?.sequence} ${lesson?.name}`}</h2>
+                    <div className={styles.video_buttons}>
+                        <a 
+                            href={``} 
+                            className={`${styles.code_btn}`}
+                            target="_blank"
+                        >Código</a>
+                        <a 
+                            href={`/slide/${urlParams.course}/${urlParams.lesson}`}
+                            className={`${styles.slide_btn}`}
+                            target="_blank"
+                        >Slide</a>
+
+                    </div>
+                </div>
+                <div className={styles.video}>
+                    <div style={{ padding: "56.25% 0 0 0", position: "relative"}}>
+                        <iframe src={lesson?.video_src} allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", border: 0 }} title="0000-intro"></iframe>
+                    </div>
+                    <script src="https://player.vimeo.com/api/player.js"></script>
+                </div>
             </div>
         </>
     );
