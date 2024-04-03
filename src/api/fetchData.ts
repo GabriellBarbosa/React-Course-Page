df60c4e47b5fcc3c89ceb27e5692e9426f8526e2
bielbarbosa900@gmail.com <bielbarbosa900@gmail.com>
2024-04-03 13:21:48 -0300

    extract function getHeadersWithNonce

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -1,11 +1,20 @@
 import { VITE_API_URL } from "../constants/enviroment";
 
 async function fetchData(endpoint: string) {
-    const response = await fetch(VITE_API_URL + endpoint);
+    const response = await fetch(VITE_API_URL + endpoint, getHeadersWithNonce());
     const json = await response.json();
     if (response.ok) 
         return json;
     throw new Error(json);
+}
+
+function getHeadersWithNonce() {
+    const nonce: string = (window as any).wp_auth?.nonce || 'not defined';
+    return {
+        headers: {
+            'X-WP-Nonce': nonce
+        }
+    }
 }
 
 export default fetchData;
