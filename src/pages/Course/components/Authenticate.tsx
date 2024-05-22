7fd1bf48c2712c3e4e46845cfa5004cdf00d00fb
GabriellBarbosa <bielbarbosa900@gmail.com>
2024-05-22 14:50:32 -0300

    remove code duplication with VideoBackground component

--- src/pages/Course/components/Authenticate.tsx
+++ src/pages/Course/components/Authenticate.tsx
@@ -1,14 +1,17 @@
 import styles from './Authenticate.module.css';
 import SubscribeButton from '../../../components/SubscribeButton';
+import VideoBackground from './VideoBackground';
 
 function Authenticate() {
     return (
-        <div className={styles.authenticate}>
-            <p>Para acessar o vídeo:</p>
-            <a href="/conta">Login</a>
-            <span>ou</span>
-            <SubscribeButton text="Inscreva-se" className={styles.subscription} />
-        </div>
+        <VideoBackground>
+            <div className={styles.wrapper}>
+                <p>Para acessar o vídeo:</p>
+                <a href="/conta">Login</a>
+                <span>ou</span>
+                <SubscribeButton text="Inscreva-se" className={styles.subscription} />
+            </div>
+        </VideoBackground>
     )
 }
 
